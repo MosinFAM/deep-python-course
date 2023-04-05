1afe77437e38e2183aeb00b6dcf2b4bcaabb6740
MosinFAM <AlexFilinXXX@mail.ru>
2023-04-05 15:12:23 +0300

    .

--- 03/test_custom_list.py
+++ 03/test_custom_list.py
@@ -5,25 +5,34 @@ from custom_list import CustomList
 
 class TestCustomList(unittest.TestCase):
     def test_sum_and_difference(self):
+
+        def compare(list1, list2):
+            for index in range(len(max(list1, list2))):
+                if list1[index] != list2[index]:
+                    return False
+            return True
+
         lst1 = CustomList([1, 2, 3, 4, 5, 6])
         lst2 = CustomList([99, 99, 99, 99])
         lst3 = CustomList([])
         lst4 = [1, 2, 3, 4, 5]
         lst5 = []
 
-        self.assertEqual(lst1 + lst2, CustomList([100, 101, 102, 103, 5, 6]))
-        self.assertEqual(lst1 + lst4, CustomList([2, 4, 6, 8, 10, 6]))
-        self.assertEqual(lst1 + lst3, CustomList([1, 2, 3, 4, 5, 6]))
-        self.assertEqual(lst3 + lst5, CustomList([]))
-        self.assertEqual(lst4 + lst1, CustomList([2, 4, 6, 8, 10, 6]))
-        self.assertEqual(lst5 + lst1, CustomList([1, 2, 3, 4, 5, 6]))
+        # for index in range(len(lst1 + lst2))
 
-        self.assertEqual(lst1 - lst2, CustomList([-98, -97, -96, -95, 5, 6]))
-        self.assertEqual(lst1 - lst4, CustomList([0, 0, 0, 0, 0, 6]))
-        self.assertEqual(lst1 - lst3, CustomList([1, 2, 3, 4, 5, 6]))
-        self.assertEqual(lst3 - lst5, CustomList([]))
-        self.assertEqual(lst4 - lst1, CustomList([0, 0, 0, 0, 0, -6]))
-        self.assertEqual(lst5 - lst1, CustomList([-1, -2, -3, -4, -5, -6]))
+        self.assertTrue(compare(lst1 + lst2, CustomList([100, 101, 102, 103, 5, 6])))
+        self.assertTrue(compare(lst1 + lst4, CustomList([2, 4, 6, 8, 10, 6])))
+        self.assertTrue(compare(lst1 + lst3, CustomList([1, 2, 3, 4, 5, 6])))
+        self.assertTrue(compare(lst3 + lst5, CustomList([])))
+        self.assertTrue(compare(lst4 + lst1, CustomList([2, 4, 6, 8, 10, 6])))
+        self.assertTrue(compare(lst5 + lst1, CustomList([1, 2, 3, 4, 5, 6])))
+
+        self.assertTrue(compare(lst1 - lst2, CustomList([-98, -97, -96, -95, 5, 6])))
+        self.assertTrue(compare(lst1 - lst4, CustomList([0, 0, 0, 0, 0, 6])))
+        self.assertTrue(compare(lst1 - lst3, CustomList([1, 2, 3, 4, 5, 6])))
+        self.assertTrue(compare(lst3 - lst5, CustomList([])))
+        self.assertTrue(compare(lst4 - lst1, CustomList([0, 0, 0, 0, 0, -6])))
+        self.assertTrue(compare(lst5 - lst1, CustomList([-1, -2, -3, -4, -5, -6])))
 
     def test_comparison(self):
         lst1 = CustomList([1, 2, 3, 4, 5, 6])
