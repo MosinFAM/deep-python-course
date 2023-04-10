54a92884aea1b8be6b8e9f08104534778889eaee
MosinFAM <AlexFilinXXX@mail.ru>
2023-04-10 20:23:46 +0300

    second chance

--- 02/parse.py
+++ 02/parse.py
@@ -1,24 +1,26 @@
 import json
 
 
-# def keyword_callback(word):
-#     return len(word)
-
-
 def parse_json(
         keyword_callback, json_str: str, required_fields=None, keywords=None
 ):
+    if keywords is None:
+        print('keywords=None')
+
     json_doc = json.loads(json_str)
     for key, value in json_doc.items():
         if required_fields is not None and key in required_fields:
             if keywords is not None:
                 for keyword in keywords:
-                    if keyword in value:
-                        keyword_callback(keyword)
+                    if keyword in value.split(' '):
+                        keyword_callback(keyword, key)
+        elif required_fields is None:
+            print('required_fields=None')
+            break
 
 
 # print(parse_json(
-#                 keyword_callback=lambda x: print(len(x)),
-#                 json_str='{"key1": "Word1 word2", "key2": "word2 word3"}',
+#                 keyword_callback=lambda x, y: print(len(x)+len(y)),
+#                 json_str='{"key1": "word1 word2", "key2": "word2 word3"}',
 #                 required_fields=["key1"],
-#                 keywords=["word2"]))
+#                 keywords=["word1"]))
--- 02/test_parse.py
+++ 02/test_parse.py
@@ -6,31 +6,92 @@ from parse import parse_json
 
 class TestParser(unittest.TestCase):
     def test_keyword_callback(self):
-        with mock.patch("parse.print") as mock_obj:
+        with mock.patch("parse.len") as mock_obj:
             parse_json(
                 mock_obj,
-                json_str='{"key1": "Word1 word2", "key2": "word2 word3"}',
-                required_fields=["key1"],
+                json_str='{"key1": "word1 word2", "key2": "word2 word3"}',
+                required_fields=[],
                 keywords=["word2"])
-            self.assertEqual(mock_obj.call_count, 1)
+            self.assertEqual(mock_obj.call_count, 0)
 
             parse_json(
                 mock_obj,
-                json_str='{"key1": "Word1 word2", "key2": "word2 word3"}',
+                json_str='{"key1": "word1 word2", "key2": "word2 word3"}',
                 required_fields=None,
                 keywords=["word2"])
-            self.assertEqual(mock_obj.call_count, 1)
+            self.assertEqual(mock_obj.call_count, 0)
+            expected_calls = []
+            self.assertEqual(expected_calls, mock_obj.mock_calls)
 
             parse_json(
                 mock_obj,
-                json_str='{"key1": "Word1 word2", "key2": "word2 word3"}',
+                json_str='{"key1": "word1 word2", "key2": "word2 word3"}',
                 required_fields=["key2"],
                 keywords=["word3"])
-            self.assertEqual(mock_obj.call_count, 2)
+            self.assertEqual(mock_obj.call_count, 1)
+            expected_calls = [
+                mock.call("word3", "key2"),
+            ]
+            self.assertEqual(expected_calls, mock_obj.mock_calls)
 
             parse_json(
                 mock_obj,
-                json_str='{"key1": "Word1 word2", "key2": "word2 word3"}',
+                json_str='{"key1": "vfdvfd dfv wev", "key2": "кактус кактусовый кактусы как кактусыы какктус"}',
+                required_fields=["key2"],
+                keywords=["кактусовый"])
+            self.assertEqual(mock_obj.call_count, 2)
+            expected_calls = [
+                mock.call("word3", "key2"),
+                mock.call("кактусовый", "key2"),
+            ]
+            self.assertEqual(expected_calls, mock_obj.mock_calls)
+
+            parse_json(
+                mock_obj,
+                json_str='{"key1": "word1 word2", "key2": "word2 word3"}',
                 required_fields=["key1"],
                 keywords=None)
             self.assertEqual(mock_obj.call_count, 2)
+            expected_calls = [
+                mock.call("word3", "key2"),
+                mock.call("кактусовый", "key2"),
+            ]
+            self.assertEqual(expected_calls, mock_obj.mock_calls)
+
+            parse_json(
+                mock_obj,
+                json_str='{"key1": "word1 word2", "key2": "word2 word3"}',
+                required_fields=None,
+                keywords=None)
+            self.assertEqual(mock_obj.call_count, 2)
+            expected_calls = [
+                mock.call("word3", "key2"),
+                mock.call("кактусовый", "key2"),
+            ]
+            self.assertEqual(expected_calls, mock_obj.mock_calls)
+
+            parse_json(
+                mock_obj,
+                json_str='{"key1": "word1 word2", "key2": "word2 word3"}',
+                required_fields=["key1"],
+                keywords=["кактусовый"])
+            self.assertEqual(mock_obj.call_count, 2)
+            expected_calls = [
+                mock.call("word3", "key2"),
+                mock.call("кактусовый", "key2"),
+            ]
+            self.assertEqual(expected_calls, mock_obj.mock_calls)
+
+            parse_json(
+                mock_obj,
+                json_str='{"key1": "word1 word2", "key2": "word2 word3"}',
+                required_fields=["key1"],
+                keywords=["word1", "word2", "word3"])
+            self.assertEqual(mock_obj.call_count, 4)
+            expected_calls = [
+                mock.call("word3", "key2"),
+                mock.call("кактусовый", "key2"),
+                mock.call("word1", "key1"),
+                mock.call("word2", "key1"),
+            ]
+            self.assertEqual(expected_calls, mock_obj.mock_calls)
