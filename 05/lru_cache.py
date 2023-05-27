c7119e85c6a369458957585a8df964630a6c71ef
MosinFAM <AlexFilinXXX@mail.ru>
2023-05-27 19:31:15 +0300

    .

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -1,24 +1,54 @@
-class LRUCache:
+class Node:
+    def __init__(self, key=None, value=None):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
-    def __init__(self, capacity):
+
+class LRUCache:
+    def __init__(self, capacity: int):
         self.capacity = capacity
         self.cache = {}
-        self.order = []
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
-    def get(self, key):
-        if key not in self.cache:
-            return None
-        self.order.remove(key)
-        self.order.append(key)
-        return self.cache[key]
-
-    def set(self, key, value):
+    def get(self, key: int):
         if key in self.cache:
-            self.order.remove(key)
-        elif len(self.cache) == self.capacity:
-            del self.cache[self.order.pop(0)]
-        self.cache[key] = value
-        self.order.append(key)
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return None
+
+    def set(self, key: int, value: int):
+        if key in self.cache:
+            node = self.cache[key]
+            node.value = value
+            self._remove(node)
+            self._add(node)
+        else:
+            if len(self.cache) == self.capacity:
+                del self.cache[self.head.next.key]
+                self._remove(self.head.next)
+            node = Node(key, value)
+            self.cache[key] = node
+            self._add(node)
+
+    def _add(self, node: Node):
+        prev_node = self.tail.prev
+        prev_node.next = node
+        node.prev = prev_node
+        node.next = self.tail
+        self.tail.prev = node
+
+    def _remove(self, node: Node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
 
 
 # cache = LRUCache(2)
@@ -27,4 +57,4 @@ class LRUCache:
 # cache.set("k2", "val2")
 # cache.set("k3", "val3")
 #
-# print(cache.get("k4"))
+# print(cache.get("k3"))
