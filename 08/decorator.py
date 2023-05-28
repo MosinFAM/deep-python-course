46f19b9f2d791a43d0cd4ed287de0a9e4a7b4568
MosinFAM <AlexFilinXXX@mail.ru>
2023-05-28 23:54:44 +0300

    .

--- 08/decorator.py
+++ 08/decorator.py
@@ -1,33 +1,27 @@
-import cProfile, pstats, io
-import functools
+import cProfile
+import pstats
+import io
+from functools import wraps
 
 
 def profile_deco(func):
-    def wrapper(*args, **kwargs):
-        pr = cProfile.Profile()
-        pr.enable()
+    profile = cProfile.Profile()
+
+    @wraps(func)
+    def deco(*args, **kwargs):
+        profile.enable()
         res = func(*args, **kwargs)
-        pr.disable()
-        # print(s.getvalue())
-        # func_name = func.__name__
-        return pr.print_stats()
+        profile.disable()
+        return res
 
-    return wrapper
-
-
-# def profile_deco(print_stats):
-#     def print_stats(func):
-#         @functools.wraps(func)
-#         def wrapper(*args, **kwargs):
-#             pr = cProfile.Profile()
-#             pr.enable()
-#             res = func(*args, **kwargs)
-#             pr.disable()
-#             return pr.print_stats()
-#         func.print_stats = print_stats
-#         return wrapper
-#
-#     return print_stats
+    def print_stat():
+        st = io.StringIO()
+        sortby = "cumulative"
+        ps = pstats.Stats(profile, stream=st).sort_stats(sortby)
+        ps.print_stats()
+        print(st.getvalue())
+    deco.print_stat = print_stat
+    return deco
 
 
 @profile_deco
@@ -44,7 +38,7 @@ if __name__ == "__main__":
     add(1, 2)
     add(4, 5)
     sub(4, 5)
-    add.print_stats()
+    add.print_stat()
 
 # add.print_stat()  # выводится результат профилирования суммарно по всем вызовам функции add (всего два вызова)
 # sub.print_stat()  # выводится результат профилирования суммарно по всем вызовам функции sub (всего один вызов)
